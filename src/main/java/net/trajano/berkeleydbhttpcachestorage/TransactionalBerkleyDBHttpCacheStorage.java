be8fdcf5a3939aac2aca981eb6765509ba0ba98b
Archimedes Trajano <developer@trajano.net>
2011-06-20 13:35:30 -0400

    corrected version of BDB Implemented transactional

--- src/main/java/net/trajano/berkeleydbhttpcachestorage/TransactionalBerkleyDBHttpCacheStorage.java
+++ src/main/java/net/trajano/berkeleydbhttpcachestorage/TransactionalBerkleyDBHttpCacheStorage.java
@@ -2,6 +2,8 @@ package net.trajano.berkeleydbhttpcachestorage;
 
 import java.io.IOException;
 
+import net.trajano.berkeleydbhttpcachestorage.internal.BerkeleyDBCacheDatabase;
+
 import org.apache.http.client.cache.HttpCacheEntry;
 import org.apache.http.client.cache.HttpCacheStorage;
 import org.apache.http.client.cache.HttpCacheUpdateCallback;
@@ -9,12 +11,13 @@ import org.apache.http.client.cache.HttpCacheUpdateException;
 
 import com.sleepycat.je.Database;
 import com.sleepycat.je.Environment;
+import com.sleepycat.je.Transaction;
 
 public class TransactionalBerkleyDBHttpCacheStorage implements HttpCacheStorage {
 	/**
-	 * Berkeley DB.
+	 * Berkeley Cache DB.
 	 */
-	private final Database database;
+	private final BerkeleyDBCacheDatabase database;
 
 	/**
 	 * Berkeley DB environment. Used to get the transaction.
@@ -31,30 +34,55 @@ public class TransactionalBerkleyDBHttpCacheStorage implements HttpCacheStorage 
 	public TransactionalBerkleyDBHttpCacheStorage(
 			final Environment environment, final Database database) {
 		this.environment = environment;
-		this.database = database;
+		this.database = new BerkeleyDBCacheDatabase(database);
 	}
 
 	public HttpCacheEntry getEntry(final String key) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		final Transaction txn = environment.beginTransaction(null, null);
+		try {
+			final HttpCacheEntry entry = database.getEntry(txn, key);
+			txn.commit();
+			return entry;
+		} catch (final IOException e) {
+			txn.abort();
+			throw e;
+		}
 	}
 
 	public void putEntry(final String key, final HttpCacheEntry entry)
 			throws IOException {
-		// TODO Auto-generated method stub
+		final Transaction txn = environment.beginTransaction(null, null);
+		try {
+			database.putEntry(txn, key, entry);
+			txn.commit();
+		} catch (final IOException e) {
+			txn.abort();
+			throw e;
+		}
 
 	}
 
 	public void removeEntry(final String key) throws IOException {
-		// TODO Auto-generated method stub
-
+		final Transaction txn = environment.beginTransaction(null, null);
+		try {
+			database.removeEntry(txn, key);
+			txn.commit();
+		} catch (final IOException e) {
+			txn.abort();
+			throw e;
+		}
 	}
 
 	public void updateEntry(final String key,
 			final HttpCacheUpdateCallback callback) throws IOException,
 			HttpCacheUpdateException {
-		// TODO Auto-generated method stub
-
+		final Transaction txn = environment.beginTransaction(null, null);
+		try {
+			database.updateEntry(txn, key, callback);
+			txn.commit();
+		} catch (final IOException e) {
+			txn.abort();
+			throw e;
+		}
 	}
-
 }
